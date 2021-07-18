3b2b76686b7f7ee86ae70156f51eff26da8582a3
ofael <fael_assis@hotmail.com>
2021-07-18 12:06:31 -0300

    My first app for notification

---
+++ app/src/main/java/app/notificationapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package app.notificationapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnSend : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSend = findViewById(R.id.btn_send_notification)
+        btnSend.setOnClickListener {
+            this.showNotification("1234","bootcamp","kotlin")
+        }
+    }
+}
